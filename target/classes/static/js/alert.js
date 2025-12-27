0f22f8f6e634bc9940c4bb02d76df1fc04369d13
bdt1301 <thienphu1301@gmail.com>
2025-12-27 19:21:57 +0700

    add Dockerfile and .gitignore; remove IDE and build artifacts from repo

--- target/classes/static/js/alert.js
+++
@@ -1,7 +0,0 @@
-setTimeout(() => {
-	const alerts = document.querySelectorAll('.alert');
-	alerts.forEach(alert => {
-		alert.classList.remove('show');
-		alert.classList.add('fade');
-	});
-}, 5000);
