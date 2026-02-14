7bda7358ccd551ec19257b6997be492cb931c2fd
bdt1301 <thienphu1301@gmail.com>
2026-02-14 09:54:48 +0700

    chore: move environment variables to .env

--- src/main/java/com/user/steammgmt/SteamMgmtApplication.java
+++ src/main/java/com/user/steammgmt/SteamMgmtApplication.java
@@ -9,21 +9,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import com.user.steammgmt.model.User;
 import com.user.steammgmt.repository.UserRepository;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SteamMgmtApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SteamMgmtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
-	@Bean
-	CommandLineRunner initUsers(UserRepository repo, PasswordEncoder encoder) {
-		return args -> {
-			if (repo.count() == 0) {
-				repo.save(new User("admin", encoder.encode("admin123"), "ROLE_ADMIN", "Admin", "admin@example.com",
-						"/images/default_avatar_admin.jpg"));
-				repo.save(new User("user", encoder.encode("user123"), "ROLE_USER", "User", "user@example.com",
-						"/images/default_avatar_user.jpg"));
-			}
-		};
-	}
+        SpringApplication.run(SteamMgmtApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initUsers(UserRepository repo, PasswordEncoder encoder) {
+        return args -> {
+            if (repo.count() == 0) {
+                repo.save(new User("admin", encoder.encode("admin123"), "ROLE_ADMIN", "Admin", "admin@example.com",
+                        "/images/default_avatar_admin.jpg"));
+                repo.save(new User("user", encoder.encode("user123"), "ROLE_USER", "User", "user@example.com",
+                        "/images/default_avatar_user.jpg"));
+            }
+        };
+    }
 }
